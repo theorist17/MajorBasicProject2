527bfe19cdf5ee92594da99ecd2ab91d1cf9e16b
unknown <theorist17>
2018-10-19 14:20:41 +0900

    something

---
+++ app/src/main/java/org/androidtown/MajorBasicProject2/PageCheckActivity.java
@@ -0,0 +1,13 @@
+package org.androidtown.MajorBasicProject2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PageCheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_check);
+    }
+}
---
+++ app/src/main/java/org/androidtown/MajorBasicProject2/PageConfirmActivity.java
@@ -0,0 +1,13 @@
+package org.androidtown.MajorBasicProject2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PageConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_confirm);
+    }
+}
